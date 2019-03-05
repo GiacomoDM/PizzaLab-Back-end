29c92b8169fe93bc813578e8478f8eb94814c793
Giacomo Del Moro <delmoro.giacomo@gmail.com>
2019-03-05 10:39:40 +0100

    category controller

---
+++ CategoryService/src/main/java/it/synclab/pizzalab/CategoryService/CategoryController.java
@@ -0,0 +1,33 @@
+package it.synclab.pizzalab.CategoryService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController {
+	
+	@Autowired
+	private CategoryService categoryService;
+	
+	@RequestMapping("/categories")
+	public List<Category> getAllProducts() {
+		return this.categoryService.getAllCategories();
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/categories")
+	public void addCategory(@RequestBody Category category) {
+		this.categoryService.addCategory(category);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/categories/{id}")
+	public void deleteProduct(@PathVariable String id) {
+		this.categoryService.deleteCategory(id);
+	}
+
+}
